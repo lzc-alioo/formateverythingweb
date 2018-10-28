2015b0612e31ea6a420955cc2c7065dd19ebfce2
liuzhichong <liuzhichong@didichuxing.com>
2018-10-28 14:00:54 +0800

    add tool log

--- cicd/deploy-web.sh
+++ cicd/deploy-web.sh
@@ -1,21 +1,28 @@
 # /export/shell/deploy.sh
 
-. /etc/profile
-
 sourcepath=/export/work/gitstudy/formateverythingweb
 sourcejar=$sourcepath/formateverythingweb-web/target/formateverythingweb-web-1.0.0-SNAPSHOT.jar
 sourcebin=$sourcepath/cicd/start-web.sh
 
-cd $sourcepath
+. /etc/profile
+. $sourcepath/cicd/ltool/*.sh
 
+log "构建项目 开始"
+
+log "构建项目 获取最新代码开始"
+cd $sourcepath
 git pull
+log "构建项目 获取最新代码结束"
+
+log "构建项目 编译最新代码开始"
 mvn clean -U install
+log "构建项目 编译最新代码结束"
 
 targetpath=/export/packages/formateverythingweb-web/`date '+%Y%m%d.%H%M%S'`
 targetjar=formateverythingweb-web-1.0.0-SNAPSHOT.jar
 mkdir -p $targetpath
 
-
+log "构建项目 复制可执行程序开始"
 cp -r $sourcejar $targetpath
 cp -r $sourcebin  $targetpath
 
@@ -25,11 +32,15 @@ else
     echo "最新版本未找到，放弃执行:$pathname$filename"
     exit 0
 fi
+log "构建项目 复制可执行程序结束"
 
-#删除掉历史版本
+log "构建项目 删除掉历史版本开始"
 find /export/packages/formateverythingweb-web// -mtime +3|xargs -exec rm -rf {} \;
+log "构建项目 删除掉历史版本结束"
 
 
 chmod 777 $targetpath/*.sh
 
 $targetpath/start-web.sh
+
+
